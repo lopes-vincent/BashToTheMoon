2775296e649253981de7bdf9db80aa07476acfb6
Vincent Lopes-Vicente <vlopesvicente@gmail.com>
2020-12-01 00:27:23 +0100

    Finish th game

---
+++ Assets/Scripts/RocketController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    public GameController gameController;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       gameController.EndGame();
+    }
+}
---
+++ Assets/Scripts/StartController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
